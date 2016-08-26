5b3245c9d1048c35ccd1e81053427f5a3b8c45f7
low0621 <low0621@gmail.com>
2016-08-26 09:56:29 +0800

    add content board

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-$('#header').click(
+$('#header>button').click(
 	function(){
+		$('#header>button').css('display','none');
 		$('#header').css('height','15%');
 		$('#header>span').css('font-size','1.8em');
 		$('#header>span').css('top','20%');
@@ -12,5 +13,47 @@ $('.item').click(
 	function(){
 		$('.item').removeClass('active');
 		$(this).addClass('active');
+	}
+)
+
+$('#announce').click(
+	function(){
+		$('.segment').removeClass('show');
+		$('#announce_content').addClass('show');
+	}
+)
+
+$('#research').click(
+	function(){
+		$('.segment').removeClass('show');
+		$('#research_content').addClass('show');
+	}
+)
+
+$('#about').click(
+	function(){
+		$('.segment').removeClass('show');
+		$('#about_content').addClass('show');
+	}
+)
+
+$('#people').click(
+	function(){
+		$('.segment').removeClass('show');
+		$('#students_content').addClass('show');
+	}
+)
+
+$('#students').click(
+	function(){
+		$('.segment').removeClass('show');
+		$('#students_content').addClass('show');
+	}
+)
+
+$('#links').click(
+	function(){
+		$('.segment').removeClass('show');
+		$('#links_content').addClass('show');
 	}
 )

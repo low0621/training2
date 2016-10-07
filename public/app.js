5a819e41402de8165c2076876e9e0b677ab7ac2b
low0621 <low0621@gmail.com>
2016-10-08 02:18:58 +0800

    link data

--- public/app.js
+++ public/app.js
@@ -54,10 +54,16 @@ $('#announce_content .item').click(function(){
 	$('.column').attr('data', $(this).attr('id').replace(/_button$/, ''))
 })
 
-$.getJSON('/do', { test: true, a: 'b' }, function(data){
-  console.log(data)
-  if(data.err === true)
-    console.log('here')
+$.getJSON('/do', function(it){
+  if(it.err)
+    return console.log(it.err)
+  console.log(it.list.announce)
+
+  let arr = it.list
+  d3.select('#news-content').selectAll('.news.info').data(arr.announce).enter().append('div').classed('news info', true).text(function(it){ return it.title })
+  d3.select('#admission-content').selectAll('.admission.info').data(arr.admission).enter().append('div').classed('admission info',true).text(function(it){return it.title})
+  d3.select('#scholarship-content').selectAll('.scholarship.info').data(arr.scholarship).enter().append('div').classed('scholarship info', true).text(function(it){ return it.title })
+  d3.select('#employment-content').selectAll('.employment.info').data(arr.employment).enter().append('div').classed('employment info', true).text(function(it){ return it.title })
 })
 
 // vi:et:nowrap:sw=2:ts=2
